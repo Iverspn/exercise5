9c82bbef2e5c6bfd6bf2205b98cb984bee4d484d
iverson <150758778+Iverspn@users.noreply.github.com>
2024-05-06 15:33:37 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function setCookie(name, value, days) {  
+    var date = new Date();  
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));  
+    var expires = "; expires=" + date.toUTCString();  
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";  
+}  
+  
+function createAccount() {  
+    var username = document.getElementById("username").value;  
+    var password = document.getElementById("password").value;  
+  
+   
+    setCookie("username", username, 7);  
+   
+    setCookie("password", password, 7);  
+  
+   
+    alert("Account created and credentials saved in cookies.");  
+}  
